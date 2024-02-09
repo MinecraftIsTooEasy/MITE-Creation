3b68ae82a3c7ec447abbd209f583be1a06126925
rusted_iron_ingot <130846228+rusted-iron-ingot@users.noreply.github.com>
2024-02-10 00:16:39 +0800

    deepslate gravel and sand gravel implementation

--- src/mod/mitecreation/mixin/block/BlockGravelMixin.java
+++ src/mod/mitecreation/mixin/block/BlockGravelMixin.java
@@ -2,15 +2,21 @@ package mod.mitecreation.mixin.block;
 
 import mod.mitecreation.item.Items;
 import net.minecraft.*;
+import net.xiaoyu233.fml.FishModLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import mod.mitecreation.block.Blocks;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(value= BlockGravel.class)
-public class BlockGravelMixin extends Block{
+public class BlockGravelMixin extends BlockFalling implements IBlockWithSubtypes{
 
     protected BlockGravelMixin(int par1, Material par2Material, BlockConstants constants) {
         super(par1, par2Material,constants);
@@ -43,7 +49,7 @@ public class BlockGravelMixin extends Block{
                 n2 = Item.chipFlint.itemID;
             }
         } else {
-            n2 = random.nextInt(3) > 0 ? Items.rawCopperNugget.itemID : (random.nextInt(3) > 0 ? Items.rawSilverNugget.itemID : (random.nextInt(3) > 0 ? Items.rawGoldNugget.itemID : (random.nextInt(3) > 0 ? Items.rawGoldNugget.itemID : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardObsidian.itemID) : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardEmerald.itemID) : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardDiamond.itemID) : (random.nextInt(3) > 0 ? Items.rawTungstenNugget.itemID : (random.nextInt(3) > 0 ? Items.rawMithrilNugget.itemID : Items.rawAdamantiumNugget.itemID))))))));
+            n2 = random.nextInt(3) > 0 ? Items.rawCopperNugget.itemID : (random.nextInt(3) > 0 ? Items.rawSilverNugget.itemID : (random.nextInt(3) > 0 ? Items.rawGoldNugget.itemID : (random.nextInt(3) > 0 ? Items.rawGoldNugget.itemID : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardObsidian.itemID) : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardEmerald.itemID) : (random.nextInt(3) > 0 ? (blockBreakInfo.wasExploded() ? -1 : Item.shardDiamond.itemID) : (random.nextInt(3) > 0 ? Items.rawMithrilNugget.itemID : Items.rawAdamantiumNugget.itemID)))))));
         }
         if (this.isNetherGravel(blockBreakInfo.getMetadata())) {
             if (n2 == Item.copperNugget.itemID || n2 == Item.silverNugget.itemID || n2 == Item.mithrilNugget.itemID || n2 == Item.adamantiumNugget.itemID) {
@@ -63,5 +69,67 @@ public class BlockGravelMixin extends Block{
     @Shadow
     private boolean isNetherGravel(int metadata) {
         return false;
+    }
+
+    @Shadow
+    private BlockSubtypes subtypes;
+
+    @Override
+    public String getMetadataNotes() {
+        return "0=Gravel, 1=Village Road, 2=Nether Gravel, 3=Deep Gravel, 4=Sand Gravel";
+    }
+
+    @Override
+    public boolean isValidMetadata(int metadata) {
+        return metadata >= 0 && metadata < 4;
+    }
+
+    @Override
+    public int getBlockSubtypeUnchecked(int metadata) {
+        if(metadata >= 4){
+            return 0;
+        }
+        return metadata;
+    }
+
+    @Override
+    public int getItemSubtype(int metadata) {
+        switch (metadata){
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            default:
+                FishModLoader.LOGGER.error("getItemSubtype():Unhandled subtype:" + metadata);
+                return 0;
+        }
+    }
+
+    @Override
+    public void a(mt par1IconRegister) {
+        this.subtypes.setIcons(this.registerIcons(par1IconRegister, this.getTextures()));
+    }
+
+    @Override
+    public IIcon a(int side, int metadata) {
+        return this.subtypes.getIcon(this.getBlockSubtype(metadata));
+    }
+
+    @Override
+    public String[] getTextures() {
+        return this.subtypes.getTextures();
+    }
+
+    @Override
+    public String[] getNames() {
+        return this.subtypes.getNames();
+    }
+    @Inject(method = "<init>",at = @At("TAIL"))
+    private void inject(CallbackInfo callbackInfo){
+        this.subtypes = new BlockSubtypes(new String[]{"gravel", "nether_gravel","deep_gravel","sand_gravel"});
     }
 }
